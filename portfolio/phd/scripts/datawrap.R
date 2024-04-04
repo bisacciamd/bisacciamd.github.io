f9110522c61a64f9161b39a842de140c23a13e30
G Bisaccia <argovarius@gmail.com>
2024-04-04 15:38:24 +0200

    datawrap

--- portfolio/phd/scripts/datawrap.R
+++ portfolio/phd/scripts/datawrap.R
@@ -1,6 +1,6 @@
-#Remote data fetch 
-#remotes::install_github('OuhscBbmc/REDCapR')
+# Remote data fetch
+# remotes::install_github('OuhscBbmc/REDCapR')
 library(REDCapR)
-api_url <- "https://redcap.synology.me/api/"
-token <- "E8065DC34088541188300BACBC7ACDF7"
+api_url <- readLines("redcap_api_url.secret")
+token <- readLines("redcap_token.secret")
 data <- redcap_read(redcap_uri = api_url, token = token)$data # uncomment to wrap
